d5bc8d9c60dc4d78df135e2ef849c0581b581c72
Jisse Reitsma <info@yireo.com>
2015-02-05 17:03:09 +0100

    Add remark + PWT styling

---
+++ expert_sessions/index.php
@@ -0,0 +1,52 @@
+<?php
+$slideGroups = array(
+    array(
+        'group' => 'Programma',
+        'style' => 'expertsession',
+        'slides' => array(
+            array('file' => 'joomla_templating/sander/indexphp', 'title' => 'Optimale index.php zonder fluff', 'speaker' => 'Sander'),
+            array('file' => 'joomla_templating/jisse/template_overrides', 'title' => 'Template Overrides', 'speaker' => 'Jisse'),
+            array('file' => 'joomla_templating/sander/jlayout', 'title' => 'JLayout', 'speaker' => 'Sander'),
+            array('file' => 'joomla_templating/hans/bootstrap', 'title' => 'Bootstrap', 'speaker' => 'Hans'),
+            array('file' => 'joomla_templating/jisse/template_overrides', 'title' => 'PHP Helper klasse', 'speaker' => 'Jisse'),
+            array('file' => 'joomla_templating/jisse/js_snippets', 'title' => 'JavaScript / jQuery', 'speaker' => 'Jisse'),
+            array('file' => 'joomla_templating/hans/less', 'title' => 'LESS', 'speaker' => 'Hans'),
+            array('file' => 'joomla_templating/jisse/building_tools', 'title' => 'Automatiseren', 'speaker' => 'Jisse'),
+        ),
+    ),
+);
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Joomla! Templating - Expert Sessie</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" href="css/pwt.css" />
+    <link rel="stylesheet" href="css/custom.css" />
+  </head>
+  <body style="background: transparent url('images/expert-sessie.jpg') top left no-repeat">
+    <div class="container">
+        <div class="title-box">
+            <div class="title">
+                <h1>Joomla! templating<br/><span>Expert Sessie</span></h1>
+                <p>vrijdag 06 februari, 14:00-17:00 uur, Almere</p>
+            </div>
+        </div>
+    </div>
+    <div class="container container-program">
+    <?php foreach($slideGroups as $slideGroup): ?>
+    <h3><?php echo $slideGroup['group']; ?></h3>
+    <ul>
+        <?php foreach($slideGroup['slides'] as $slide) : ?>
+        <li>
+            <?php $title = $slide['title'] . ' (' . $slide['speaker']. ')'; ?>
+            <a href="slide.php?style=<?= $slideGroup['style'] ?>&slide=<?= $slide['file'] ?>&title=<?= $slide['title']; ?>">
+                <?php echo $title; ?>
+            </a>
+        </li>
+        <?php endforeach; ?>
+    </ul>
+    <?php endforeach; ?>
+    </div>
+  </body>
+</html>
---
+++ expert_sessions/slide.php
@@ -0,0 +1,31 @@
+<?php
+// Fetch parameters
+$title = $_GET['title'];
+$style = $_GET['style'];
+$slide = $_GET['slide'];
+
+// Security checks
+$style = preg_replace('/([^a-zA-Z0-9\_\.\-]+)/', '', $style);
+$slide = preg_replace('/([^a-zA-Z0-9\/\_\.\-]+)/', '', $slide);
+$slide = realpath('./'.$slide.'.md');
+if(empty($slide)) die('no slide');
+if(stristr($slide, __DIR__) == false) die('access denied');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?php echo $title; ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" href="css/<?php echo $style; ?>.css" />
+  </head>
+  <body>
+    <textarea id="source"><?php echo file_get_contents($slide); ?></textarea>
+    <script src="https://gnab.github.io/remark/downloads/remark-latest.min.js"></script>
+    <script>
+      var slideshow = remark.create({
+        slideNumberFormat: '<?php echo $title; ?> - %current% of %total%',
+        highlightStyle: 'googlecode'
+      });
+    </script>
+  </body>
+</html>

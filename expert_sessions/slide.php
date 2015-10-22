84de8ba2b882a7c8415aaa0b269c99168cf7237a
Sander Potjer <github@sanderpotjer.nl>
2015-10-22 23:19:07 +0200

    Video player

--- expert_sessions/slide.php
+++ expert_sessions/slide.php
@@ -4,20 +4,41 @@ $slideshow = new Slideshow();
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <title><?php echo $slideshow->getTitle(); ?></title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <link rel="stylesheet" href="css/pwt.css" />
-    <link rel="stylesheet" href="css/<?php echo $slideshow->getStyle(); ?>.css" />
-  </head>
-  <body>
-    <textarea id="source"><?php echo file_get_contents($slideshow->getFile()); ?></textarea>
-    <script src="js/remark-latest.min.js"></script>
-    <script>
-      var slideshow = remark.create({
-        slideNumberFormat: '<?php echo $slideshow->getTitle(); ?> - %current% of %total%',
-        highlightStyle: 'googlecode'
-      });
-    </script>
-  </body>
+<head>
+	<title><?php echo $slideshow->getTitle(); ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<link rel="stylesheet" href="css/pwt.css"/>
+	<link rel="stylesheet" href="css/plyr.css"/>
+	<link rel="stylesheet" href="css/<?php echo $slideshow->getStyle(); ?>.css"/>
+</head>
+<body>
+<textarea id="source"><?php echo file_get_contents($slideshow->getFile()); ?></textarea>
+<script src="js/remark-latest.min.js"></script>
+<script>
+	var slideshow = remark.create({
+		slideNumberFormat: '<?php echo $slideshow->getTitle(); ?> - %current% of %total%',
+		highlightStyle: 'googlecode'
+	});
+</script>
+<script>
+	(function(d, p){
+		var a = new XMLHttpRequest(),
+			b = d.body;
+		a.open("GET", p, true);
+		a.send();
+		a.onload = function(){
+			var c = d.createElement("div");
+			c.style.display = "none";
+			c.innerHTML = a.responseText;
+			b.insertBefore(c, b.childNodes[0]);
+		}
+	})(document, "images/sprite.svg");
+</script>
+<script src="js/plyr.js"></script>
+<script>plyr.setup({
+		controls: ["restart", "play", "current-time", "duration", "fullscreen"],
+		displayDuration: false,
+		volume: 1,
+	});</script>
+</body>
 </html>
